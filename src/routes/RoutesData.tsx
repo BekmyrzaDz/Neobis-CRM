66d0c1b3b07477dffb99bea8d879fed10553e1d1
Khamza Avazbekov <khamzadevv@gmail.com>
2023-03-07 18:42:17 +0600

    fix : fix structure

--- src/routes/RoutesData.tsx
+++ src/routes/RoutesData.tsx
@@ -1,10 +1,12 @@
 import { createBrowserRouter } from 'react-router-dom';
+import AnalyticsPage from '../pages/analitics/components/AnalyticsPage';
 import ArchivePage from '../pages/archive/components/ArchivePage';
 import AuthPage from '../pages/auth/AuthPage';
 import CoursePage from '../pages/course/components/CoursePage';
 import EmployeesPage from '../pages/Employees/EmployeesPage';
 import Layout from '../pages/Layout/components/Layout';
 import MainPage from '../pages/main/components/MainPage/MainPage';
+import Page404 from '../pages/page404/Page404';
 import StudentsPage from '../pages/students/components/StudentsPage';
 import WaitingListPage from '../pages/waitingList/components/WaitingListPage';
 
@@ -22,13 +24,18 @@ export const router = createBrowserRouter([
     path: '/',
     element: <Layout />,
     children: [
-      { index: true, element: <MainPage /> },
+      // { index: true, element: <MainPage /> },
+      { path: 'home-page', element: <MainPage /> },
       { path: 'students', element: <StudentsPage /> },
       { path: 'employees', element: <EmployeesPage /> },
       { path: 'courses', element: <CoursePage /> },
-      { path: 'analytics', element: <EmployeesPage /> },
+      { path: 'analytics', element: <AnalyticsPage /> },
       { path: 'waiting-list', element: <WaitingListPage /> },
       { path: 'archive', element: <ArchivePage /> },
     ],
+  },
+  {
+    path: '*',
+    element: <Page404 />,
   },
 ]);
